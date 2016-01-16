8e7891dca9e1d65662845ef73bcfb575cd180ad2
0u812 <0u812@users.noreply.github.com>
2016-01-15 16:58:49 -0800

    update docs

--- docs/search/variables_7.js
+++ docs/search/variables_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['padding',['padding',['../struct____fr__options.html#ad53b16a5835fe8cdac6554a1ed31f89e',1,'__fr_options']]],
   ['prerandomize',['prerandomize',['../struct____fr__options.html#acf4e38810a454707d3c14e082a90cebc',1,'__fr_options']]]
 ];
