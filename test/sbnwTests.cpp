78d8d73ba7fffa2539c6f515aee4f77e336bae96
Ciaran Welsh <cwelsh2@uw.edu>
2020-03-01 23:54:39 -0800

    new test

--- test/sbnwTests.cpp
+++ test/sbnwTests.cpp
@@ -4,51 +4,28 @@
 
 
 #include "gtest/gtest.h"
+#include <filesystem>
 #include "SBNWClientAPI.h"
 
 #include <iostream>
 
+
+
 using namespace std;
 
 
-TEST(SBNWSharedTests, t) {
+
+
+TEST(SBNWSharedTests, TestVersion) {
     SBNWClientAPI sbnw;
     const char *version = sbnw.gf_getCurrentLibraryVersion();
     const char *expected = "1.3.27";
     ASSERT_STREQ(expected, version);
 }
 
-//TEST_F(SBNWSharedTestFixture, t) {
-//    auto gf_getCurrentLibraryVersion = (gf_getCurrentLibraryVersionPtr) GetProcAddress(
-//            handle, "gf_getCurrentLibraryVersion");
-//    if (gf_getCurrentLibraryVersion == nullptr) {
-//        GetLastError();
-//        throw std::invalid_argument("deaded");
-//    }
-//    const char *version = gf_getCurrentLibraryVersion();
-//    if (version == nullptr) {
-//        GetLastError();
-//    }
-//    ASSERT_STREQ("1.3.27", version);
-//}
-//
-//TEST_F(SBNWSharedTestFixture, TestGetFunction) {
-//    auto gf_getCurrentLibraryVersion = getFunction<gf_getCurrentLibraryVersionPtr>(
-//            "gf_getCurrentLibraryVersion");
-//    if (gf_getCurrentLibraryVersion == nullptr) {
-//        GetLastError();
-//    }
-//    const char *version = gf_getCurrentLibraryVersion();
-//    const char *expected = "1.3.27";
-//    cout << version << endl;
-//    ASSERT_STREQ(expected, version);
-//}
-//
-//TEST_F(SBNWSharedTestFixture, TestGetVersion) {
-//    const char *version = gf_getCurrentLibraryVersion();
-////    const char* version = getVersion();
-////    cout << gf_getCurrentLibraryVersion << endl;
-//    cout << version << endl;
-////    const char *expected = "1.3.27";
-////    ASSERT_STREQ(expected, version);
-//}
+TEST(SBNWSharedTests, TestLoadSbml) {
+    SBNWClientAPI sbnw;
+    gf_SBMLModel* sbml = sbnw.gf_loadSBMLfile(R"(D:\sbnw\test\teusink2000.xml)");
+    ASSERT_NE(sbml->pdoc, nullptr );
+}
+
